5830c04326ef466a653d2f093dad19c6a46d0eea
bradjacobs <bradjacobs@gmail.com>
2021-06-06 22:00:36 -0700

    for mgecs, move detection of first dataLine into helper method.

--- src/main/java/com/github/bradjacobs/stock/classifications/mgecs/MgecsDataConverter.java
+++ src/main/java/com/github/bradjacobs/stock/classifications/mgecs/MgecsDataConverter.java
@@ -15,7 +15,7 @@ public class MgecsDataConverter extends BaseDataConverter<MgecsRecord>
     private static final int GROUP_ID_LENGTH = 5;
     private static final int INDUSTRY_ID_LENGTH = 8;
 
-    private static final String START_LINE_INDICATOR = "1 Cyclical"; // todo - address this
+    private static final String START_LINE_INDICATOR = "1 Cyclical"; // the string to look for to detect the first line with the 'data'
 
     @Override
     public Classification getClassification()
@@ -33,101 +33,94 @@ public class MgecsDataConverter extends BaseDataConverter<MgecsRecord>
 
         MgecsRecord currentRecord = null;
 
-        boolean parsingStarted = false;
-        for (int i = 0; i < pdfFileLines.length; i++)
+        // search for line where teh data 'actually' starts.
+        int firstDataLineIndex = findFirstDataRowIndex(pdfFileLines);
+
+        for (int i = firstDataLineIndex; i < pdfFileLines.length; i++)
         {
-            // todo - clean up start detectiion.
             String line = pdfFileLines[i].trim();
-            if (!parsingStarted) {
-                if (line.equalsIgnoreCase(START_LINE_INDICATOR)) {
-                    parsingStarted = true;
-                }
+
+            if (StringUtils.isEmpty(line)) {
+                continue;
+            }
+            else if (isMorningstarLine(line)) {
+                continue;
             }
 
-            if (parsingStarted)
+            if (StringUtils.isNumeric(line))
             {
-                if (StringUtils.isEmpty(line)) {
-                    continue;
-                }
-                else if (isMorningstarLine(line)) {
-                    continue;
-                }
+                String id = line;
+                String name = "";
 
-                if (StringUtils.isNumeric(line))
+                // note:  page numbers are example of case that would pass 'isNumeric' check, but not pass if-check below
+                if (id.length() == SECTOR_ID_LENGTH || id.length() == GROUP_ID_LENGTH || id.length() == INDUSTRY_ID_LENGTH)
                 {
-                    String id = line;
-                    String name = "";
+                    name = pdfFileLines[++i];  // don't trim (..yet)
 
-                    // note:  page numbers are example of case that would pass 'isNumeric' check, but not pass if-check below
-                    if (id.length() == SECTOR_ID_LENGTH || id.length() == GROUP_ID_LENGTH || id.length() == INDUSTRY_ID_LENGTH)
+                    // note: the name affiliated w/ the group category can be split across multiple lines
+                    if (id.length() == GROUP_ID_LENGTH)
                     {
-                        name = pdfFileLines[++i];  // don't trim (..yet)
+                        // SIDE: this is squirrelly
+                        //   Problem is that sometimes text goes to next line and the '\n' is the ONLY space character b/w 2 words.
+                        //   Thus have to take extra caution so that 2 seperate words don't get concatenated into 1.
 
-                        // note: the name affiliated w/ the group category can be split across multiple lines
-                        if (id.length() == GROUP_ID_LENGTH)
+
+                        // temporarily add back new line and don't trim
+                        //  b/c spacing can get messed up.
+                        String futureLine = pdfFileLines[i+1];
+
+                        while (! StringUtils.isNumeric(futureLine.trim()))
                         {
-                            // SIDE: this is squirrelly
-                            //   Problem is that sometimes text goes to next line and the '\n' is the ONLY space character b/w 2 words.
-                            //   Thus have to take extra caution so that 2 seperate words don't get concatenated into 1.
-
-
-                            // temporarily add back new line and don't trim
-                            //  b/c spacing can get messed up.
-                            String futureLine = pdfFileLines[i+1];
-
-                            while (! StringUtils.isNumeric(futureLine.trim()))
-                            {
-                                if (StringUtils.isNotEmpty(futureLine.trim())) {
-                                    name = name + futureLine;
-                                }
-
-                                // note:  increment the 'current' i value,
-                                // then the futureLine is the next line after teh current line  (thus the +1)
-                                i++;
-                                futureLine = pdfFileLines[i+1];
-                            }
-                            name = cleanWhitespace(name);
-                        }
-
-                        // now finally can trim + clean value
-                        name = cleanValue(name);
-
-
-                        if (id.length() == SECTOR_ID_LENGTH)
-                        {
-                            if (currentRecord != null) {
-                                recordList.add(currentRecord);
-                            }
-                            currentRecord = new MgecsRecord();
-                            currentRecord.setSectorId(id);
-                            currentRecord.setSectorName(name);
-                        }
-                        else if (id.length() == GROUP_ID_LENGTH)
-                        {
-                            if (currentRecord.getIndustryGroupId() != null) {
-                                recordList.add(currentRecord);
-                                currentRecord = currentRecord.copy();
-                                currentRecord.setIndustryId(null);
-                                currentRecord.setIndustryName(null);
-                            }
-                            currentRecord.setIndustryGroupId(id);
-                            currentRecord.setIndustryGroupName(name);
-                        }
-                        else if (id.length() == INDUSTRY_ID_LENGTH)
-                        {
-                            if (currentRecord.getIndustryId() != null) {
-                                recordList.add(currentRecord);
-                                currentRecord = currentRecord.copy();
+                            if (StringUtils.isNotEmpty(futureLine.trim())) {
+                                name = name + futureLine;
                             }
 
-                            // NOTE: sub-optimal soln b/c future lines are examined in getDescription method as well as this loop.
-                            //   (even though problem exists, not worth addressing at present)
-                            String desc = getDescription(pdfFileLines, i+1);
-                            currentRecord.setDescription(desc);
-
-                            currentRecord.setIndustryId(id);
-                            currentRecord.setIndustryName(name);
+                            // note:  increment the 'current' i value,
+                            // then the futureLine is the next line after teh current line  (thus the +1)
+                            i++;
+                            futureLine = pdfFileLines[i+1];
                         }
+                        name = cleanWhitespace(name);
+                    }
+
+                    // now finally can trim + clean value
+                    name = cleanValue(name);
+
+
+                    if (id.length() == SECTOR_ID_LENGTH)
+                    {
+                        if (currentRecord != null) {
+                            recordList.add(currentRecord);
+                        }
+                        currentRecord = new MgecsRecord();
+                        currentRecord.setSectorId(id);
+                        currentRecord.setSectorName(name);
+                    }
+                    else if (id.length() == GROUP_ID_LENGTH)
+                    {
+                        if (currentRecord.getIndustryGroupId() != null) {
+                            recordList.add(currentRecord);
+                            currentRecord = currentRecord.copy();
+                            currentRecord.setIndustryId(null);
+                            currentRecord.setIndustryName(null);
+                        }
+                        currentRecord.setIndustryGroupId(id);
+                        currentRecord.setIndustryGroupName(name);
+                    }
+                    else if (id.length() == INDUSTRY_ID_LENGTH)
+                    {
+                        if (currentRecord.getIndustryId() != null) {
+                            recordList.add(currentRecord);
+                            currentRecord = currentRecord.copy();
+                        }
+
+                        // NOTE: sub-optimal soln b/c future lines are examined in getDescription method as well as this loop.
+                        //   (even though problem exists, not worth addressing at present)
+                        String desc = getDescription(pdfFileLines, i+1);
+                        currentRecord.setDescription(desc);
+
+                        currentRecord.setIndustryId(id);
+                        currentRecord.setIndustryName(name);
                     }
                 }
             }
@@ -151,6 +144,20 @@ public class MgecsDataConverter extends BaseDataConverter<MgecsRecord>
 
         return recordList;
     }
+
+
+    private int findFirstDataRowIndex(String[] fileLines)
+    {
+        for (int i = 0; i < fileLines.length; i++)
+        {
+            String line = fileLines[i].trim();
+            if (line.equalsIgnoreCase(START_LINE_INDICATOR)) {
+                return i;
+            }
+        }
+        return -1;  // not found.
+    }
+
 
     /**
      * check if line contains "Morningstar", which implies it's a header/footer
