6cd95421a3d267fa2607934c252ea41dc254f3ee
bradjacobs <bradjacobs@gmail.com>
2021-11-13 18:17:58 -0800

    fix trbc parse bug if name on multiple lines

--- src/main/java/com/github/bradjacobs/stock/classifications/trbc/TrbcDataConverter.java
+++ src/main/java/com/github/bradjacobs/stock/classifications/trbc/TrbcDataConverter.java
@@ -31,27 +31,6 @@ public class TrbcDataConverter extends BaseDataConverter<TrbcRecord>
         return parseLines(pdfFileLines);
     }
 
-
-    private boolean isTableColumnHeaderRow(String line)
-    {
-        return line.startsWith("Economic");
-    }
-
-    private boolean isSkipableLine(String line)
-    {
-        if (isTableColumnHeaderRow(line)) {
-            return true;
-        }
-        if (line.startsWith("Thomson Reuters")) {
-            return true;
-        }
-        if (line.toLowerCase().contains("refinitiv")) {
-            return true;
-        }
-        return false;
-    }
-
-
     private List<TrbcRecord> parseLines(String[] lines)
     {
         List<TrbcRecord> recordList = new ArrayList<>();
@@ -68,34 +47,11 @@ public class TrbcDataConverter extends BaseDataConverter<TrbcRecord>
         String industryId = "";
         String activityId = "";
 
-        for (int i = 0; i < lines.length; i++)
+        List<String[]> lineElementList = getParsableLineChunks(lines);
+        for (String[] lineElements : lineElementList)
         {
-            String line = lines[i].trim();
-
-            if (isSkipableLine(line)) {
-                continue;
-            }
-
-            String[] lineElements = line.split(" ");
-
-            if (lineElements.length <= 2) {
-                continue;
-            }
-
-            String trbcId = lineElements[lineElements.length-1];
-            if (! StringUtils.isNumeric(trbcId)) {
-                continue;
-            }
-
-            String permId = lineElements[lineElements.length-2];  //  note: can be "{New}" (i.e. can't assume it's a number)
-            int permIdIndex = line.indexOf(permId);
-
-            // name is immediately before the permId
-            String name = line.substring(0, permIdIndex-1);
-
-            name = cleanValue(name);
-
-            // todo - fix couple rare exceptions where name is on more than one line.
+            String name = cleanValue(lineElements[0]);
+            String trbcId = lineElements[1];
 
             if (trbcId.length() == ECONOMIC_SECTOR_ID_LENGTH) {
                 economicSectorId = trbcId;
@@ -135,8 +91,82 @@ public class TrbcDataConverter extends BaseDataConverter<TrbcRecord>
     protected String cleanValue(String input)
     {
         // todo - some minor upper/lower case fixes.
-
         return super.cleanValue(input);
+    }
+
+
+    /**
+     * Return a list of 'valid' line pairs
+     *   each String[] has length == 2
+     *       String[0] = name,   String[1] = trbcId
+     * @param lines
+     * @return
+     */
+    private List<String[]> getParsableLineChunks(String[] lines)
+    {
+        List<String[]> resultList = new ArrayList<>();
+
+        for (int i = 0; i < lines.length; i++)
+        {
+            String line = lines[i].trim();
+            if (isSkipableLine(line)) {
+                continue;
+            }
+
+            String[] lineElements = line.split(" ");
+
+            // if have exactly 2 elements and the last is a number
+            //   then this is a result of a pdf multi-line parse issue
+            //     thus prepend the previous 2 lines to create a 'new' line
+            if (lineElements.length == 2) {
+
+                if (StringUtils.isNumeric(lineElements[1])) {
+                    line = String.join(" ", lines[i-2], lines[i-1], line);
+                    lineElements = line.split(" ");
+                }
+            }
+
+            if (lineElements.length > 2)
+            {
+                String trbcId = lineElements[lineElements.length-1];
+                if (! StringUtils.isNumeric(trbcId)) {
+                    continue;
+                }
+
+                //  note: permId can be "{New}" (i.e. can't assume it's a number)
+                String permId = lineElements[lineElements.length-2];
+                int permIdIndex = line.indexOf(permId);
+
+                // name is immediately before the permId
+                String name = line.substring(0, permIdIndex-1);
+                resultList.add(new String[]{name, trbcId});
+            }
+        }
+
+        return resultList;
+    }
+
+
+    private boolean isSkipableLine(String line)
+    {
+        if (StringUtils.isEmpty(line)) {
+            return true;
+        }
+        if (isTableColumnHeaderRow(line)) {
+            return true;
+        }
+        if (line.startsWith("Thomson Reuters")) {
+            return true;
+        }
+        if (line.toLowerCase().contains("refinitiv")) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isTableColumnHeaderRow(String line)
+    {
+        return line.startsWith("Economic");
     }
 
 }
