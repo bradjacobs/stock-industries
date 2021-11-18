28067d9d5ea032f14a1646fe82ead520c2d3aacc
bradjacobs <bradjacobs@gmail.com>
2021-11-17 20:17:40 -0800

    nasdaq refactoring

--- src/main/java/com/github/bradjacobs/stock/classifications/nasdaq/NasdaqDataConverter.java
+++ src/main/java/com/github/bradjacobs/stock/classifications/nasdaq/NasdaqDataConverter.java
@@ -6,11 +6,14 @@ import com.github.bradjacobs.stock.MapperBuilder;
 import com.github.bradjacobs.stock.classifications.Classification;
 import com.github.bradjacobs.stock.classifications.DataConverter;
 import com.github.bradjacobs.stock.util.DownloadUtil;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -25,14 +28,16 @@ import java.util.TreeSet;
  *   1. was unable to find 'official' document explaining hierarchy (thus have 'faked' the id values)
  *   2. these sector/industries show up on many other places (not just Nasdaq)
  *
+ * UPDATE:
+ *   upon review, skeptical of putting much trust in original sectoer/industry data
  */
 public class NasdaqDataConverter implements DataConverter<NasdaqRecord>
 {
     private static final String SECTOR_KEY = "sector";
     private static final String INDUSTRY_KEY = "industry";
-    private static final String TICKER_KEY = "symbol"; // <- only need for diagnostic purposes.
+    private static final String TICKER_KEY = "symbol"; // technically only need for diagnostic purposes.
 
-    // the following tickers had a "unique permutation" of Sector/Industry that _subjectively_ looked suspicious
+    // the following tickers had a "unique permutation" of Sector/Industry that __subjectively__ looked suspicious
     //   thus suppressing for now.  There are other tickets with unique Sector/Industry that look correct
     //   and have been left in.
     private static final Set<String> SKIPPABLE_TICKERS = new HashSet<>(Arrays.asList(
@@ -41,8 +46,6 @@ public class NasdaqDataConverter implements DataConverter<NasdaqRecord>
             "YGMZ", // Sector: Consumer Services  Industry: Transportation Services
             "ZTO"   // Sector: Transportation     Industry: Advertising
     ));
-
-    private static final boolean PRINT_UNIQUE_TICKER_SECTOR_INDUSTRY = false;  // for debugging only
 
     @Override
     public Classification getClassification()
@@ -55,32 +58,68 @@ public class NasdaqDataConverter implements DataConverter<NasdaqRecord>
     {
         String json = DownloadUtil.downloadFile(getClassification().getSourceFileLocation());
 
+        // using TreeMap/TreeSet to keep everything sorted.
+        //   { "Sector" : { "Industry" : [(all ticker values for sector/industry]  }  }
+        Map<String, Map<String,Set<String>>> sectorIndustryMap = new TreeMap<>();
+
         JsonMapper mapper = MapperBuilder.json().build();
         String innerRowsJson = mapper.writeValueAsString(mapper.readTree(json).get("data").get("rows"));
         List<Map<String,String>> listOfMaps = mapper.readValue(innerRowsJson, new TypeReference<List<Map<String, String>>>() {});
 
-        // using TreeMap & TreeSet to keep everything sorted.
-        // key   = sector
-        // value =   Map... key = industry,  value = all tickers for the sector/industry
-        Map<String, Map<String,Set<String>>> sectorIndustryMap = new TreeMap<>();
-
-        for (Map<String, String> tickerMap : listOfMaps)
+        for (Map<String, String> tickerRecordMap : listOfMaps)
         {
-            String ticker = tickerMap.get(TICKER_KEY);
-            String sector = tickerMap.get(SECTOR_KEY);
-            String industry = tickerMap.get(INDUSTRY_KEY);
+            String ticker = tickerRecordMap.get(TICKER_KEY);
+            String sector = tickerRecordMap.get(SECTOR_KEY);
+            String industry = tickerRecordMap.get(INDUSTRY_KEY);
 
             if (SKIPPABLE_TICKERS.contains(ticker)) {
                 continue;
             }
 
-            if (StringUtils.isNotEmpty(sector) && StringUtils.isNotEmpty(industry))
-            {
+            if (StringUtils.isNotEmpty(sector) && StringUtils.isNotEmpty(industry)) {
                 Map<String, Set<String>> industryMap = sectorIndustryMap.computeIfAbsent(sector, k -> new TreeMap<>());
                 Set<String> tickerSet = industryMap.computeIfAbsent(industry, k -> new TreeSet<>());
                 tickerSet.add(ticker);
             }
         }
+
+        //   NOTE: uncomment for debugging purposes
+        //sectorMapValidityCheck(sectorIndustryMap);
+
+        List<NasdaqRecord> recordList = new ArrayList<>();
+
+        // counters are used to give 'pseudo' id values.
+        int sectorCounter = 0;
+        int industryCounter = 0;
+
+        for (Map.Entry<String, Map<String, Set<String>>> sectorEntry : sectorIndustryMap.entrySet())
+        {
+            String sector = sectorEntry.getKey();
+            String sectorId = String.valueOf(++sectorCounter);
+
+            Set<String> industrySet = sectorEntry.getValue().keySet();
+            industryCounter = 0;
+            for (String industry : industrySet) {
+                String industryId = String.format("%s.%d", sectorId, ++industryCounter);
+                recordList.add(new NasdaqRecord(sectorId, sector, industryId, industry));
+            }
+        }
+
+        return recordList;
+    }
+
+
+
+    //////////////////////////////////
+
+    /**
+     * Debug Use:
+     * Prints any one-off looking sector/industry pair, b/c sometimes the data can be 'off'
+     * @param sectorIndustryMap sectorIndustryMap
+     */
+    private void sectorMapValidityCheck(Map<String, Map<String,Set<String>>> sectorIndustryMap)
+    {
+        List<SectorIndustryTickerCountRecord> trackingList = new ArrayList<>();
 
         // scan over the entire data structure to see if there are any Sector/Industry combinations
         //   that correlate with exactly _ONE_ ticker.  Meaning there 'could' be a data error.
@@ -93,6 +132,8 @@ public class NasdaqDataConverter implements DataConverter<NasdaqRecord>
             {
                 String industry = industryEntry.getKey();
                 Set<String> tickerSet = industryEntry.getValue();
+                trackingList.add(new SectorIndustryTickerCountRecord(sector, industry, tickerSet.size()));
+
                 if (tickerSet.size() == 1)
                 {
                     msgBuilder.append(String.format("| Ticker: %-5s | Sector: %-21s | Industry: %-39s |\n", tickerSet.iterator().next(), sector, industry));
@@ -100,31 +141,40 @@ public class NasdaqDataConverter implements DataConverter<NasdaqRecord>
             }
         }
 
-        if (msgBuilder.length() > 0 && PRINT_UNIQUE_TICKER_SECTOR_INDUSTRY)
+        if (msgBuilder.length() > 0)
         {
             System.out.println("NOTE: Nasdaq data has following sector/industry pairs only once...");
             System.out.println(msgBuilder.toString());
             System.out.println();
         }
 
-        List<NasdaqRecord> recordList = new ArrayList<>();
-
-        int sectorCounter = 0;
-        for (Map.Entry<String, Map<String, Set<String>>> sectorEntry : sectorIndustryMap.entrySet())
-        {
-            String sector = sectorEntry.getKey();
-            String sectorId = String.valueOf(++sectorCounter);
-
-            Set<String> industrySet = sectorEntry.getValue().keySet();
-            int industryCounter = 0;
-            for (String industry : industrySet)
-            {
-                String industryId = String.format("%s.%d", sectorId, ++industryCounter);
-                recordList.add(new NasdaqRecord(sectorId, sector, industryId, industry));
+        // dump info of industries in more than 1 sector.  // this only counts for 2 dupes... there could be more.
+        Map<String, SectorIndustryTickerCountRecord> industryRecordMap = new HashMap<>();
+        for (SectorIndustryTickerCountRecord record : trackingList) {
+            SectorIndustryTickerCountRecord existingRecord = industryRecordMap.get(record.industry);
+            if (existingRecord != null) {
+                System.out.printf("%-40s -- [Sector] %-25s[%3d]  vs  [Sector] %-25s[%3d]%n",
+                        record.industry,
+                        record.sector,
+                        record.tickerCount,
+                        existingRecord.sector,
+                        existingRecord.tickerCount);
+            }
+            else {
+                industryRecordMap.put(record.industry, record);
             }
         }
+    }
 
-        return recordList;
+    private static class SectorIndustryTickerCountRecord {
+        private final String sector;
+        private final String industry;
+        private final int tickerCount;
+        public SectorIndustryTickerCountRecord(String sector, String industry, int tickerCount) {
+            this.sector = sector;
+            this.industry = industry;
+            this.tickerCount = tickerCount;
+        }
     }
 
 
